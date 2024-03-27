bdef0322b5b02cfdc3af006e26679b459f6e3724
MR HΛRSH <157881731+Harsh123499@users.noreply.github.com>
2024-03-27 12:02:20 +0530

    Update play.py

--- DAXXMUSIC/utils/inline/play.py
+++ DAXXMUSIC/utils/inline/play.py
@@ -60,10 +60,10 @@ def stream_markup_timer(_, chat_id, played, dur):
             )
         ],
         [
-            InlineKeyboardButton(text="ⅠⅠ ", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="‣‣|", callback_data=f"ADMIN Skip|{chat_id}"),
-        ], [InlineKeyboardButton(text="▷", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="ⅠⅠ ᴘᴀᴜsᴇ", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="▢ sᴛᴏᴘ", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="sᴋɪᴘ ‣‣|", callback_data=f"ADMIN Skip|{chat_id}"),
+        ], [InlineKeyboardButton(text="ʀᴇᴘʟᴀʏ ↻", callback_data=f"ADMIN Stop|{chat_id}"),
             InlineKeyboardButton(text="Sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/Sigma_Byz"),
            ]
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -74,10 +74,10 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="ⅠⅠ", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="‣‣|", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="ⅠⅠ ᴘᴀᴜsᴇ", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="▢ sᴛᴏᴘ", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="sᴋɪᴘ ‣‣|", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="ʀᴇᴘʟᴀʏ ↻", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
             
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
