871f72ad98edc84657d8c833371848ed5b9927a8
Lost Venom <149289075+LostVenom@users.noreply.github.com>
2024-03-26 23:10:16 -0700

    Update play.py

--- DAXXMUSIC/utils/inline/play.py
+++ DAXXMUSIC/utils/inline/play.py
@@ -63,9 +63,9 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="ⅠⅠ ", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="‣‣|", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        
+        ], [InlineKeyboardButton(text="▷", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="Sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/Sigma_Byz"),
+           ]
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
