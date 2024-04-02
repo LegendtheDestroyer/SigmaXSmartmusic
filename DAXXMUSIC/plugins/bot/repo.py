bba8f4fcc82dc9fcff4eac9ace1d91d955d6e661
MR HΛRSH <157881731+Harsh123499@users.noreply.github.com>
2024-04-02 16:58:57 +0530

    Update repo.py

--- DAXXMUSIC/plugins/bot/repo.py
+++ DAXXMUSIC/plugins/bot/repo.py
@@ -25,7 +25,7 @@ start_txt = """**
 
 
 
-@app.on_message(filters.command("repo"))
+@app.on_message(filters.command("Lundlegakya"))
 async def start(_, msg):
     buttons = [
         [ 
@@ -80,7 +80,7 @@ InlineKeyboardButton("𝐂𝐂 𝐁𝐎𝐓", url=f"https://github.com/DAXXTEAM/
 # --------------
 
 
-@app.on_message(filters.command("repo", prefixes="#"))
+@app.on_message(filters.command("Lundlegakya", prefixes="#"))
 @capture_err
 async def repo(_, message):
     async with httpx.AsyncClient() as client:
